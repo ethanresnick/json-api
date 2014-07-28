ethanresnick/json-api

commit bd4269ed7e72c66e0a3401394f4d1173e380755e

Fix BaseController regression
BaseController isn’t a class, so sendResources can’t be declared as a
“static class method” (with @). Doing so was messing up the compiling.

lib/BaseController.ls
@@ -7,7 +7,7 @@ module.exports =
    * controllers that extend this one.
    */
   extend: (newProps) ->
-    Object.create(this, {[k, {value: v, enumerable: true}] for own k, v of newProps});
+    Object.create(@, {[k, {value: v, enumerable: true}] for own k, v of newProps});
 
   /**
    * A function that, when called, returns a new object that implements 
@@ -65,7 +65,7 @@ module.exports =
     @_buildQuery(req).promise!
       .then(->, ->)
 
-  @sendResources = (res, resources, meta) ->
+  sendResources: (res, resources, meta) ->
     if resources.type === "errors"
       if resources instanceof Collection
         status = resources.resources[0].attrs.status

build/lib/BaseController.js
@@ -6,87 +6,86 @@
   Document = require('./types/Document');
   Collection = require('./types/Collection');
   ErrorResource = require('./types/ErrorResource');
-  module.exports = [
-    {
-      /**
-       * Returns an object with newProps as properties (all enumerable) 
-       * and values and `this` in the prototype chain. Allows you create
-       * controllers that extend this one.
-       */
-      extend: function(newProps){
-        var k, v;
-        return Object.create(this, (function(){
-          var ref$, own$ = {}.hasOwnProperty, results$ = {};
-          for (k in ref$ = newProps) if (own$.call(ref$, k)) {
-            v = ref$[k];
-            results$[k] = {
-              value: v,
-              enumerable: true
-            };
-          }
-          return results$;
-        }()));
-      }
-      /**
-       * A function that, when called, returns a new object that implements 
-       * the Adapter interface. Should be provided by the child controller.
-       * Whe need to get a new adapter on each request so the query state is
-       * specific to this request (since the controller objects themselves
-       * persist between requests).
-       */,
-      adapterFn: null,
-      _buildQuery: function(req){
-        var query, ids;
-        query = this.adapterFn();
-        switch (req.method.toUpperCase()) {
-        case "GET":
-          if (req.params.id) {
-            ids = req.params.id.split(",");
-            if (ids.length > 1) {
-              query.withIds(ids);
-            } else {
-              query.withId(ids[0]);
-            }
-          } else {
-            query.any();
-          }
-          if (req.query.sort) {
-            query.sort(req.query.sort.split(','));
-          }
-          if (req.query.fields) {
-            query.onlyFields(req.query.fields.split(','));
-          }
-          break;
-        case "POST":
+  module.exports = {
+    /**
+     * Returns an object with newProps as properties (all enumerable) 
+     * and values and `this` in the prototype chain. Allows you create
+     * controllers that extend this one.
+     */
+    extend: function(newProps){
+      var k, v;
+      return Object.create(this, (function(){
+        var ref$, own$ = {}.hasOwnProperty, results$ = {};
+        for (k in ref$ = newProps) if (own$.call(ref$, k)) {
+          v = ref$[k];
+          results$[k] = {
+            value: v,
+            enumerable: true
+          };
         }
-        return query;
-      },
-      GET: function(req, res, next){
-        var after;
-        after = bind$(this, 'afterQuery');
-        return this._buildQuery(req).promise().then(function(result){
-          if (result instanceof Collection) {
-            result.resources.map(function(it){
-              return after(it, req, res);
-            });
-            return result;
+        return results$;
+      }()));
+    }
+    /**
+     * A function that, when called, returns a new object that implements 
+     * the Adapter interface. Should be provided by the child controller.
+     * Whe need to get a new adapter on each request so the query state is
+     * specific to this request (since the controller objects themselves
+     * persist between requests).
+     */,
+    adapterFn: null,
+    _buildQuery: function(req){
+      var query, ids;
+      query = this.adapterFn();
+      switch (req.method.toUpperCase()) {
+      case "GET":
+        if (req.params.id) {
+          ids = req.params.id.split(",");
+          if (ids.length > 1) {
+            query.withIds(ids);
           } else {
-            return after(result, req, res);
+            query.withId(ids[0]);
           }
-        }).then(function(resources){
-          return constructor.sendResources(res, resources);
-        })['catch'](function(err){
-          var er;
-          er = ErrorResource.fromError(err);
-          return constructor.sendResources(res, er.status, er);
-        });
-      },
-      POST: function(req, res, next){
-        var before;
-        before = bind$(this, 'beforeSave');
-        return this._buildQuery(req).promise().then(function(){}, function(){});
+        } else {
+          query.any();
+        }
+        if (req.query.sort) {
+          query.sort(req.query.sort.split(','));
+        }
+        if (req.query.fields) {
+          query.onlyFields(req.query.fields.split(','));
+        }
+        break;
+      case "POST":
       }
-    }, this.sendResources = function(res, resources, meta){
+      return query;
+    },
+    GET: function(req, res, next){
+      var after;
+      after = bind$(this, 'afterQuery');
+      return this._buildQuery(req).promise().then(function(result){
+        if (result instanceof Collection) {
+          result.resources.map(function(it){
+            return after(it, req, res);
+          });
+          return result;
+        } else {
+          return after(result, req, res);
+        }
+      }).then(function(resources){
+        return constructor.sendResources(res, resources);
+      })['catch'](function(err){
+        var er;
+        er = ErrorResource.fromError(err);
+        return constructor.sendResources(res, er.status, er);
+      });
+    },
+    POST: function(req, res, next){
+      var before;
+      before = bind$(this, 'beforeSave');
+      return this._buildQuery(req).promise().then(function(){}, function(){});
+    },
+    sendResources: function(res, resources, meta){
       var status;
       if (deepEq$(resources.type, "errors", '===')) {
         if (resources instanceof Collection) {
@@ -99,7 +98,7 @@
       }
       return res.json(status, new Document(resources, meta).get());
     }
-  ];
+  };
   /*
   
   

commit 52f794e4ccb316262d85a7ee901fd300190e7965

Fix BaseController responses
Problem introduced from bd4269ed7e72c66e0a3401394f4d1173e380755e

lib/BaseController.ls
@@ -70,11 +70,11 @@ module.exports =
           result
         else
           after(result, req, res)
-      ).then((resources) ->
-        @@sendResources(res, resources)
-      ).catch((err) ->
+      ).then((resources) ~>
+        @sendResources(res, resources)
+      ).catch((err) ~>
         er = ErrorResource.fromError(err)
-        @@sendResources(res, er)
+        @sendResources(res, er)
       )
 
   POST: (req, res, next) ->
@@ -82,7 +82,7 @@ module.exports =
     @_buildQuery(req).promise!
       .then(->, ->)
 
-#create, update, read, list, delete    
+#todo: create, update, delete    
 /*
 
   mongooseDocsToJsonApiResponse: function(mongooseDocs) {