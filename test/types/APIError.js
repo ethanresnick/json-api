61cf994ae374bca9202622753c80c06cba898a49
Ethan Resnick <ethan.resnick@gmail.com>
2015-03-16 00:44:44 -0700

    ES6 APIError, with tests

---
+++ test/types/APIError.js
@@ -0,0 +1,34 @@
+import mocha from "mocha"
+import sinon from "sinon"
+import chai from "chai"
+import APIError from "../../lib/types/APIError"
+
+var expect = chai.expect
+
+describe("Error Objects", () => {
+  describe("validation", () => {
+    var er = new APIError(300, 1401);
+
+    it("should coerce the status to a string", () => {      
+      expect(er.status === "300").to.be.true;
+    });
+
+    it("should coerce the code to a string", () => {
+      expect(er.code === "1401").to.be.true;
+    });
+  })
+
+  describe("the fromError helper", () => {
+    it("should use the error's statusCode val as status if status not defined", () => {
+      var er = APIError.fromError({'statusCode':300});
+      expect(er.status === "300").to.be.true;
+
+      er = APIError.fromError({'status': 200, 'statusCode':300});
+      expect(er.status === "200").to.be.true;
+    });
+
+    it("should default to status 500", () => {
+      expect(APIError.fromError({}).status).to.equal("500");
+    })
+  })
+})
