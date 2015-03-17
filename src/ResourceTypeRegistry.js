aea327119a53164db41f9ca600212716547b1873
Ethan Resnick <ethan.resnick@gmail.com>
2015-03-16 18:00:05 -0700

    ES6 ResourceTypeRegistry, with tests
    Note the different semantics around registering (you can now define
    properties on a type one by one, without registering it all at once)
    and url templates (their paths now no longer need to be scoped to the
    type name, and getting them returns them by type).

---
+++ src/ResourceTypeRegistry.js
@@ -0,0 +1,89 @@
+import Q from 'q'
+import mongoose from 'mongoose'
+import Document from './types/Document'
+
+/**
+ * To fulfill a JSON API request, you often need to know about all the resources
+ * in the system--not just the primary resource associated with the type being 
+ * requested. For example, if the request is for a User, you might need to 
+ * include related Projects, so the code handling the users request needs access
+ * to the Project resource's beforeSave and afterQuery methods. Similarly, it 
+ * would need access to url templates that point at relationships on the Project
+ * resources. Etc. So we handle this by introducing a ResourceTypeRegistry that 
+ * the Dispatcher can have access to. Each resource type is registered by its 
+ * JSON api type and has a number of properties defining it.
+ */
+export default class ResourceTypeRegistry {
+  constructor() {
+    this._resourceTypes = {};
+  }
+
+  type(type, description) {
+    if(description) {
+      this._resourceTypes[type] = {};
+
+      // Set all the properties for the type that the description provides.
+      ["adapter", "beforeSave", "afterQuery", "labelToIdOrIds", 
+       "urlTemplates", "defaultIncludes", "info", "parentType"].forEach((k) => {
+        if(Object.prototype.hasOwnProperty.call(description, k)) {
+          this[k](type, description[k]);
+        }
+      });
+    }  
+    else if(this._resourceTypes[type]) {
+      return import$({}, this._resourceTypes[type]);
+    }
+  }
+
+  types() {
+    return Object.keys(this._resourceTypes)
+  }
+
+  urlTemplates(type, templates) {
+    this._resourceTypes[type] = this._resourceTypes[type] || {};
+
+    switch(arguments.length) {
+      case 1:
+        return this._resourceTypes[type].urlTemplates;
+
+      case 0:
+        var templates = {};
+        for(let type in this._resourceTypes) {
+          templates[type] = import$({}, this._resourceTypes[type].urlTemplates || {});
+        }
+        return templates;
+
+      default:
+        this._resourceTypes[type].urlTemplates = templates
+    } 
+  }
+}
+
+ResourceTypeRegistry.prototype.adapter = makeGetterSetter('adapter');
+ResourceTypeRegistry.prototype.beforeSave = makeGetterSetter('beforeSave');
+ResourceTypeRegistry.prototype.afterQuery = makeGetterSetter('afterQuery');
+ResourceTypeRegistry.prototype.labelToIdOrIds = makeGetterSetter('labelToIdOrIds');
+ResourceTypeRegistry.prototype.defaultIncludes = makeGetterSetter('defaultIncludes');
+ResourceTypeRegistry.prototype.info = makeGetterSetter('info');
+ResourceTypeRegistry.prototype.parentType = makeGetterSetter('parentType');
+
+
+function import$(obj, src){
+  var own = {}.hasOwnProperty;
+  for (var key in src) if (own.call(src, key)) obj[key] = src[key];
+  return obj;
+}
+
+function makeGetterSetter(attrName) {
+  return function(type, optValue) {
+    this._resourceTypes[type] = this._resourceTypes[type] || {};
+
+    if(optValue) {
+      return this._resourceTypes[type][attrName] = optValue;
+    }
+
+    else {
+      return this._resourceTypes[type][attrName];
+    }
+  }
+}
---
+++ test/ResourceTypeRegistry.js
@@ -0,0 +1,75 @@
+import mocha from "mocha"
+import sinon from "sinon"
+import chai from "chai"
+import ResourceTypeRegistry from "../src/ResourceTypeRegistry"
+
+var expect = chai.expect;
+var registry = {}
+var makeGetterSetterTest = function(newThing, type, methodName, deep) {
+  return function() {
+    expect(registry[methodName](type)).to.be.undefined;
+    registry[methodName](type, newThing)
+    
+    // You may get a copy of the set object back, not a direct
+    // reference. And that's acceptable. A deep check lets that pass.
+    if(deep) {
+      expect(registry[methodName](type)).to.deep.equal(newThing);
+    }
+    else {
+      expect(registry[methodName](type)).to.equal(newThing);
+    } 
+  }
+}
+
+describe("ResourceTypeRegistry", function() {
+  beforeEach(() => {
+    registry = new ResourceTypeRegistry()
+  });
+
+  describe("type", () => {
+    var description = {
+      adapter: {},
+      beforeSave: () => {},
+      afterQuery: () => {},
+      info: {},
+      urlTemplates: {'path': 'test template'}
+    };
+
+    it("should be a getter/setter for a type",
+      makeGetterSetterTest(description, "mytypes", "type", true)
+    )
+  });
+
+  describe("adapter", () => {
+    it("should be a getter/setter for a type's adapter", 
+      makeGetterSetterTest({'a':'new model'}, "mytypes", "adapter")
+    );
+  });
+
+  describe("beforeSave", () => {
+    it("should be a getter/setter for a type for a type's beforeSave", 
+      makeGetterSetterTest(() => {}, "mytypes", "beforeSave")
+    );
+  });
+
+  describe("afterQuery", () => {
+    it("should be a getter/setter for a type's afterQuery", 
+      makeGetterSetterTest(() => {}, "mytypes", "afterQuery")
+    );
+  });
+
+  describe("info", () => {
+    it("should be a getter/setter for a type's info", 
+      makeGetterSetterTest({}, "mytypes", "info")
+    )
+  });
+
+  describe("urlTemplates", () => {
+    it("should be a getter/setter for a type's urlTemplates",
+      makeGetterSetterTest(
+        {'path': 'test template'}, 
+        "mytypes", "urlTemplates", true
+      )
+    )
+  });
+});
