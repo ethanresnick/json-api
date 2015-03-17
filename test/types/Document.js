a89f059b6a45cc098036cfa5fc48e9159ea90d82
Ethan Resnick <ethan.resnick@gmail.com>
2015-03-16 22:01:02 -0700

    Cleanup

--- test/types/Document.js
+++ test/types/Document.js
@@ -64,59 +64,15 @@ describe("Document class", () => {
         .to.deep.equal([{"id": "32", "type": "people", "name": "ethan"}]);
     });
 
+    it.skip("Should include appropriate top-level links.");
   });
 
 });
+
 /* TEST CASES
 0 resources | toOne or toMany relationship, included or referenced by href
   - { type:[] }
   
-1 resource | toOne relationship | referenced by href
-  - {
-    type: {
-      "attrName": "val", 
-      "links": {
-        propName: {
-          "type": "type2", 
-          "id": "id2",  //not strictly necessary, but nice to include
-          "href": "url"
-        } 
-      }
-    }
-  }
-
-1 resource | toMany relationship | referenced by href
-  - {
-    type: {
-      "attrName": "val", 
-      "links": {
-        propName: {
-          "type": "type2",
-          "ids": ["id2", "id3"]  //not strictly necessary, but nice to include
-          "href": "url"
-        } 
-      }
-    }
-  }
-
-1 resource | toOne relationship | included.
-  - {
-      type: {
-        "attrName": "val", 
-        "links": {
-          "propName": {
-            "type": "type2", 
-            id: "id2"
-          } 
-        }
-      },
-      linked: {
-        type2: [{
-          //full resource, sans type + href but with id.
-        }]
-      }
-    }
-
 1 resource | toMany relationship | included
   - {
     type: {
@@ -152,54 +108,6 @@ describe("Document class", () => {
         },
         ...
       ]
-    }
-
-[1, n] resources | toMany relationship | referenced by href
-  - {
-      //identical to the included case below, sans the linked key
-      links: {
-        "type.propName": {
-          type: "type2",
-          href: "/type2/id"
-        }
-      },
-      type: [
-        {
-          "attrName": "val", 
-          "links": {
-            "propName": ["id2", "id3"]
-          }
-        },
-        ...
-      ]
-    }
-
-[1, n] resources | toOne relationship | included
-  - {
-      //add links so you dont have to specify type at each resource-level
-      //links key. Has some overhead--most notably having to include the
-      //url template, which isn't actually used--but pays off if the 
-      //response has more than a handful of resources.
-      links: {
-        "type.propName": {
-          type: "type2",
-          href: "/type2/id"
-        }
-      },
-      type: [
-        {
-          "attrName": "val", 
-          "links": {
-            "propName": "id2" 
-          }
-        },
-        ...
-      ],
-      linked: {
-        type2: [{
-          //full resource, sans type + href but with id.
-        }]
-      }
     }
 
 [1, n] resources | toMany relationship | included
