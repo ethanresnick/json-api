88b8f3ba9c386ac3577d46d3ed9e35f680657bb5
Ethan <ethanresnick@Ethans-MacBook-Pro-2.local>
2015-03-18 13:16:30 -0700

    First draft of Pipeline, MongooseAdapter, key steps

---
+++ src/steps/do-query/do-find.js
@@ -0,0 +1,59 @@
+import APIError from "../../types/APIError";
+
+export default function(requestContext, responseContext, registry) {
+  let type    = requestContext.type;
+  let adapter = registry.adapter(type);
+  let fields, sorts, includes, filters;
+
+  // Handle fields, sorts, includes and filters.
+  if(!requestContext.aboutLinkObject) {
+    fields = parseFields(requestContext.queryParams.fields);
+    sorts  = parseSorts(requestContext.queryParams.sort);
+    includes = parseCommaSeparatedParam(requestContext.queryParams.includes);
+    if(!includes) {
+      includes = registry.defaultIncludes(type);
+    }
+  }
+
+  return adapter
+    .find(type, requestContext.idOrIds, fields, sorts, includes, filters)
+    .then((resources) => {
+      [responseContext.primary, responseContext.included] = resources;
+    });
+}
+
+function parseSorts(sortParam) {
+  if(!sortParam) {
+    return undefined;
+  }
+  else {
+    let sorts = parseCommaSeparatedParam(sortParam);
+    let invalidSorts = sorts.filter((it) => ["+", "-"].indexOf(it.charAt(0)) === -1);
+    if(invalidSorts.length) {
+      throw new APIError(
+        400, null,
+        "All sort parameters must start with a + or a -.",
+        `The following sort parameters were invalid: $(invalidSorts.join(', ')).`
+      );
+    }
+    return sorts;
+  }
+}
+
+function parseFields(fieldsParam) {
+  let fields;
+  if(typeof fieldsParam === "object") {
+    fields = {};
+    let isField = (it) => ["id", "type", "meta"].indexOf(it) === -1;
+
+    for(let type in fieldsParam) {
+      let allowed = parseCommaSeparatedParam(fieldsParam[type]).filter(isField);
+      fields[type] = allowed;
+    }
+  }
+  return fields;
+}
+
+function parseCommaSeparatedParam(it) {
+  return it ? it.split(",").map(decodeURIComponent) : undefined;
+}
---
+++ src/steps/pre-query/label-to-ids.js
@@ -0,0 +1,32 @@
+import Collection from "../../types/Collection"
+import Q from "q"
+
+export default function(registry, req, requestContext, responseContext) {
+  return Q.Promise(function(resolve, reject) {
+    let type     = requestContext.type;
+    let adapter  = registry.adapter(type);
+    let model    = adapter.getModel(adapter.constructor.getModelName(type));
+    let idMapper = registry.labelToIdOrIds(type);
+
+    if(typeof idMapper === "function") {
+      Q(idMapper(requestContext.idOrIds, model, req)).then((newId) => {
+        let newIdIsEmptyArray = (Array.isArray(newId) && newId.length === 0);
+
+        requestContext.idOrIds = newId;
+
+        if(newId === null || newId === undefined || newIdIsEmptyArray) {
+          responseContext.primary = (newId) ? new Collection() : null;
+        }
+
+        resolve();
+      });
+    }
+
+    else {
+      resolve();
+    }
+  });
+}
+
+
+
