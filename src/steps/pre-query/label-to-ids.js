4d54e20d7ddbd01e5f1b6c3da708473fd6a360fc
Ethan Resnick <ethan.resnick@gmail.com>
2015-03-21 05:40:18 -0700

    Label to Ids: check that request allows label

--- src/steps/pre-query/label-to-ids.js
+++ src/steps/pre-query/label-to-ids.js
@@ -3,28 +3,33 @@ import Q from "q"
 
 export default function(registry, frameworkReq, requestContext, responseContext) {
   return Q.Promise(function(resolve, reject) {
-    let type      = requestContext.type;
-    let adapter   = registry.adapter(type);
-    let model     = adapter.getModel(adapter.constructor.getModelName(type));
-    let idMappers = registry.labelMappers(type);
-    let idMapper  = idMappers && idMappers[requestContext.idOrIds];
-
-    if(typeof idMapper === "function") {
-      Q(idMapper(model, frameworkReq)).then((newId) => {
-        let newIdIsEmptyArray = (Array.isArray(newId) && newId.length === 0);
-
-        requestContext.idOrIds = newId;
-
-        if(newId === null || newId === undefined || newIdIsEmptyArray) {
-          responseContext.primary = (newId) ? new Collection() : null;
-        }
-
-        resolve();
-      });
-    }
-
-    else {
+    if(!requestContext.allowLabel) {
       resolve();
+    }
+    else {
+      let type      = requestContext.type;
+      let adapter   = registry.adapter(type);
+      let model     = adapter.getModel(adapter.constructor.getModelName(type));
+      let idMappers = registry.labelMappers(type);
+      let idMapper  = idMappers && idMappers[requestContext.idOrIds];
+
+      if(typeof idMapper === "function") {
+        Q(idMapper(model, frameworkReq)).then((newId) => {
+          let newIdIsEmptyArray = (Array.isArray(newId) && newId.length === 0);
+
+          requestContext.idOrIds = newId;
+
+          if(newId === null || newId === undefined || newIdIsEmptyArray) {
+            responseContext.primary = (newId) ? new Collection() : null;
+          }
+
+          resolve();
+        });
+      }
+
+      else {
+        resolve();
+      }
     }
   });
 }
