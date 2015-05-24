6f9495fa5bf0f1fcefcbc6160630e3f2c41712b7
Ethan Resnick <ethan.resnick@gmail.com>
2015-05-24 01:50:19 -0400

    Resource type: refactor and bugfixes
    The prior field validation approach didn’t work, because we were trying
    to look inside _attrs and _relationships as objects before we
    initialized them to empty objects. But, rather than fixing that bug, I
    just decided not to validate for overlapping attr/relationship names
    (the user can do that while building the api, rather than on each
    request). And I cleaned this way up at the same time.

--- src/types/Resource.js
+++ src/types/Resource.js
@@ -6,48 +6,13 @@ export default class Resource {
       [type, id, attrs, relationships, meta];
   }
 
-  removeAttr(attrPath) {
-    if(this._attrs) {
-      deleteNested(attrPath, this._attrs);
-    }
+  get id() {
+    return this._id;
   }
 
-  get attrs() {
-    return this._attrs;
-  }
-
-  set attrs(attrs) {
-    if(typeof attrs !== "object" || Array.isArray(attrs)) {
-      throw new Error("Attributes must be an object.");
-    }
-
-    for(let name in attrs) {
-      if(this._isValidField(name)) {
-        this._attrs[name] = attrs[name];
-      }
-      else {
-        throw new Error(`${name} is an invalid or duplicate attribute name.`);
-      }
-    }
-  }
-
-  get relationships() {
-    return this._relationships;
-  }
-
-  set relationships(relationships) {
-    if(typeof relationships !== "object" || Array.isArray(relationships)) {
-      throw new Error("Relationships must be an object.");
-    }
-
-    for(let name in relationships) {
-      if(this._isValidField(name)) {
-        this._relationships[name] = relationships[name];
-      }
-      else {
-        throw new Error(`${name} is an invalid or duplicate relationship name.`);
-      }
-    }
+  set id(id) {
+    // allow empty id for the case of a new resource POST.
+    this._id = (id) ? String(id) : undefined;
   }
 
   get type() {
@@ -55,27 +20,64 @@ export default class Resource {
   }
 
   set type(type) {
-    validateType(type);
+    if(!type) {
+      throw new Error("type is required");
+    }
+
     this._type = String(type);
   }
 
-  get id() {
-    return this._id;
+  equals(otherResource) {
+    return this.id === otherResource.id && this.type == otherResource.type;
   }
 
-  set id(id) {
-    // allow empty id, e.g. for the case of a new resource
-    // posted from the client and not yet saved.
-    this._id = (id) ? String(id) : undefined;
+  get attrs() {
+    return this._attrs;
   }
 
-  _isValidField(name) {
-    return name !== "id" && name !== "type" &&
-      typeof this._relationships[name] !== "undefined" &&
-      typeof this._attrs[name] !== "undefined";
+  set attrs(attrs) {
+    validateFieldGroup(attrs);
+    this._attrs = attrs;
+  }
+
+  get relationships() {
+    return this._relationships;
+  }
+
+  set relationships(relationships) {
+    validateFieldGroup(relationships);
+    this._relationships = relationships;
+  }
+
+  removeAttr(attrPath) {
+    if(this._attrs) {
+      deleteNested(attrPath, this._attrs);
+    }
+  }
+
+  removeRelationship(relationshipPath) {
+    if(this._relationships) {
+      deleteNested(relationshipPath, this._relationships);
+    }
   }
 }
 
-function validateType(type) {
-  if(!type) throw new Error("type is required");
+/**
+ * Checks that a group of fields (i.e. the attributes or the relationships
+ * objects) are provided as objects and that they don't contain `type` and `id`
+ * members. Note: this doesn't check that attributes and relationships don't
+ * contain the same keys as one another, since it only operates on one group
+ * at a time.
+ *
+ * @throws {Error} If the group isn't an object or it has `type` or `id` keys.
+ * @returns void
+ */
+function validateFieldGroup(group) {
+  if(typeof group !== "object" || Array.isArray(group)) {
+    throw new Error("Attributes and relationships must be provided as an object.");
+  }
+
+  if(typeof group.id !== "undefined" && typeof group.type !== "undefined") {
+    throw new Error("`type` and `id` cannot be used as attribute or relationship names.");
+  }
 }
