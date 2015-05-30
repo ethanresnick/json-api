e04244fd443e4a06db86537f1679374419399ee3
Ethan Resnick <ethan.resnick@gmail.com>
2015-05-29 20:00:37 -0700

    Rewrite content negotiation logic

---
+++ src/steps/http/content-negotiation/negotiate-content-type.js
@@ -0,0 +1,64 @@
+import Q from "q";
+import Negotiator from "negotiator";
+import APIError from "../../../types/APIError";
+import {arrayValuesMatch} from "../../../util/arrays";
+import {objectIsEmpty} from "../../../util/type-handling";
+
+/**
+ * Negotiate the Content-Type to use for the response.
+ *
+ * More arguments may be added to the function later to support
+ * negotiating on parameters for extensions.
+ *
+ * @param {String} acceptHeader The raw `Accept` header string from the client.
+ * @param {Array[String]} availableBaseTypes A list of "type/subtype"
+ *    representations, without that the server can produce for the
+ *    requested resource.
+ * @return {Promise} A Promise that resolves to the Content-Type to use,
+ *    or false if no acceptable one exists.
+ */
+export default function(acceptHeader, availableBaseTypes) {
+  return Q.Promise(function(resolve, reject) {
+    const negotiator = new Negotiator({headers: {accept: acceptHeader}});
+    const hasParams = (it) => !objectIsEmpty(it.params);
+
+    // If an endpoint supports JSON API's media type, it implicity
+    // supports JSON too. Though we'll only respond with JSON if *necessary*.
+    const endpointSupportsJsonApi = availableBaseTypes.indexOf("application/vnd.api+json") !== -1;
+    const syntheticAvailableBaseTypes = endpointSupportsJsonApi
+      ? ["application/json"].concat(availableBaseTypes)
+      : availableBaseTypes;
+
+    // Take a first stab at finding the preferred type with negotiator,
+    // but then we'll only use that type below if it's *not* json api,
+    // because we can't rely on negotiator to reason propery about parameters.
+    const acceptables = negotiator.mediaTypes(undefined, true);
+    const preferredType = negotiator.mediaType(syntheticAvailableBaseTypes);
+
+    // Find all the Accept clauses that specifically reference json api.
+    const jsonApiRanges = acceptables.filter((it) =>
+      it.type == "application" && it.subtype == "vnd.api+json"
+    );
+
+    // If we do have JSON API in the Accept header and all instances
+    // are parameterized, this is explicitly a 406.
+    if(jsonApiRanges.length && jsonApiRanges.every(hasParams)) {
+      reject(new APIError(406, null, "Not Acceptable"));
+    }
+
+    // For everything but the JSON API media type, trust
+    // negotiator to handle things correctly.
+    else if(preferredType !== "application/vnd.api+json") {
+      resolve(preferredType);
+    }
+
+    // Otherwise, our preferred type is non existent or json api and, if it's
+    // json api, we have it unparameterized at least once.
+    else if(jsonApiRanges.length && endpointSupportsJsonApi) {
+      resolve("application/vnd.api+json");
+    }
+    else {
+      reject(new APIError(406, null, "Not Acceptable"));
+    }
+  });
+}
---
+++ test/unit/steps/http/content-negotiation/negotiate-content-type.js
@@ -0,0 +1,65 @@
+import {expect} from "chai";
+import Q from "q";
+import negotiate from "../../../../../src/steps/http/content-negotiation/negotiate-content-type";
+
+describe("negotiateContentType", () => {
+  it("should return a promise", () => {
+    expect(Q.isPromise(negotiate())).to.be.true;
+  });
+
+  it("should 406 if all json api parameter instances are parameterized, even if there's a valid alternative", (done) => {
+    let accept = 'application/vnd.api+json; ext="ext2", application/json';
+    negotiate(accept, ["application/vnd.api+json"]).then(done, (err) => {
+      expect(err.status).to.equal("406");
+      done();
+    }).done();
+  })
+
+  it("should allow parameterized json api media ranges as long as not all are parameterized", (done) => {
+    let accept = 'application/vnd.api+json; ext="ext2",application/vnd.api+json';
+    negotiate(accept, ["application/vnd.api+json", "text/html"]).then((contentType) => {
+      if(contentType === "application/vnd.api+json") {
+        done();
+      }
+      else {
+        done(new Error("Should have negotiated JSON API base type"));
+      }
+    }, done).done();
+  });
+
+  it("should resolve with a non-json-api type when its the highest priority + supported by the endpoint", (done) => {
+    let accept = 'text/html,application/vnd.api+json;q=0.5,application/json';
+    negotiate(accept, ["application/vnd.api+json", "text/html"]).then((contentType) => {
+      if(contentType === "text/html") {
+        done();
+      }
+      else {
+        done(new Error("Expected HTML"));
+      }
+    }, done).done();
+  });
+
+  it("should resolve with json-api type if that's the highest priority, even if the endpoint supports an alternative", (done) => {
+    let accept = 'application/vnd.api+json,application/json,text/*';
+    negotiate(accept, ["application/vnd.api+json", "text/html"]).then((contentType) => {
+      if(contentType === "application/vnd.api+json") {
+        done();
+      }
+      else {
+        done(new Error("Expected Json API content type."));
+      }
+    }, done).done();
+  });
+
+  it("should use json if client accepts only json", (done) => {
+    let accept = "text/html,application/xhtml+xml,application/json;q=0.9,**;q=0.8";
+    negotiate(accept, ["application/vnd.api+json"]).then((contentType) => {
+      if(contentType === "application/json") {
+        done();
+      }
+      else {
+        done(new Error("Expected JSON Content Type"));
+      }
+    }, done);
+  });
+});
