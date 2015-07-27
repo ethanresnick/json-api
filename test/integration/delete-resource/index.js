46956a4b3343864f6d3404f60f72c9f507b63b6f
Andrew Branch <andrewbranch@mail.com>
2015-07-26 17:19:38 -0700

    Don't use mongoose in delete integration test

--- test/integration/delete-resource/index.js
+++ test/integration/delete-resource/index.js
@@ -1,31 +1,34 @@
 import {expect} from "chai";
 import AgentPromise from "../../app/agent";
-import mongoose from "mongoose";
+import { VALID_SCHOOL_RESOURCE_NO_ID } from "../fixtures/creation";
 
 describe("Deleting a resource", () => {
 
-  let id;
+  let Agent, id;
   before(done => {
-    AgentPromise.then(Agent => {
-      mongoose.models.Organization.create({ name: "Delete me" }, (err, model) => {
-        if (err) {
-          done(err);
-        }
-        id = model._id;
-        Agent.request("DEL", `/organizations/${id}`)
-          .type("application/vnd.api+json")
-          .send()
-          .promise()
-          .then(() => done(), done).catch(done);
-      });
-    });
+    AgentPromise.then(A => {
+      Agent = A;
+      return Agent.request("POST", "/schools")
+        .type("application/vnd.api+json")
+        .send({"data": VALID_SCHOOL_RESOURCE_NO_ID})
+        .promise()
+        .then(response => {
+          id = response.body.data.id;
+          return Agent.request("DEL", `/schools/${id}`)
+            .type("application/vnd.api+json")
+            .send()
+            .promise();
+        }, done).then(() => done(), done);
+    }, done).catch(done);
   });
 
   it("should delete a resource by id", done => {
-    mongoose.models.Organization.findById(id, (err, model) => {
-      expect(err).to.be.null;
-      expect(model).to.be.null;
-      done();
-    });
+    Agent.request("GET", `/schools/${id}`)
+      .accept("application/vnd.api+json")
+      .promise()
+      .then(done, err => {
+        expect(err.response.statusCode).to.equal(404);
+        done();
+      }).catch(done);
   });
 });
