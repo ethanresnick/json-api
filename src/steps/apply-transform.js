ethanresnick/json-api

commit c2d709d73e0c668544ed926b1aedc65d9624daaf

Allow transforms to return Promises

src/steps/apply-transform.js
@@ -1,5 +1,6 @@
 import Resource from "../types/Resource";
 import Collection from "../types/Collection";
+import { Promise } from "q";
 
 /**
  * @param toTransform Could be a single resource, a collection, a link object, or null.
@@ -11,16 +12,16 @@ export default function(toTransform, mode, registry, frameworkReq, frameworkRes)
 
   else if (toTransform instanceof Collection) {
     // below, allow the user to return undefined to remove a vlaue.
-    let newResources = toTransform.resources.map((it) =>
+    return Promise.all(toTransform.resources.map((it) =>
       transform(it, frameworkReq, frameworkRes, mode, registry)
-    ).filter((it) => it !== undefined);
-
-    return new Collection(newResources);
+    ).filter((it) => it !== undefined)).then((newResources) => {
+      return new Collection(newResources);
+    });
   }
 
   // We only transform resources or collections.
   else {
-    return toTransform;
+    return Promise.resolve(toTransform);
   }
 }
 
@@ -43,5 +44,11 @@ function transform(resource, req, res, transformMode, registry) {
     }
   };
 
-  return transformFn ? transformFn(resource, req, res, superFn) : resource;
+  if (!transformFn) {
+    return Promise.resolve(resource);
+  }
+
+  // Allow user to return a Promise or a value
+  let transformed = transformFn(resource, req, res, superFn);
+  return Promise.resolve(transformed);
 }

src/controllers/API.js
@@ -78,7 +78,7 @@ class APIController {
             yield validateRequestResources(request.type, parsedPrimary, registry);
           }
 
-          request.primary = applyTransform(
+          request.primary = yield applyTransform(
             parsedPrimary, "beforeSave", registry, frameworkReq, frameworkRes
           );
         }
@@ -154,11 +154,11 @@ class APIController {
       }
 
       // apply transforms pre-send
-      response.primary = applyTransform(
+      response.primary = yield applyTransform(
         response.primary, "beforeRender", registry, frameworkReq, frameworkRes
       );
 
-      response.included = applyTransform(
+      response.included = yield applyTransform(
         response.included, "beforeRender", registry, frameworkReq, frameworkRes
       );
 

test/app/src/resource-descriptions/organizations.js
@@ -2,5 +2,9 @@ module.exports = {
   urlTemplates: {
     "self": "http://127.0.0.1:3000/organizations/{id}",
     "relationship": "http://127.0.0.1:3000/organizations/{ownerId}/links/{path}"
+  },
+  beforeSave: function(resource) {
+    resource.attrs.description = "Added a description in beforeSave";
+    return resource;
   }
 };

test/app/src/resource-descriptions/schools.js
@@ -1,3 +1,5 @@
+import { Promise } from "q";
+
 module.exports = {
   parentType: "organizations",
   urlTemplates: {
@@ -19,5 +21,12 @@ module.exports = {
         "description": "Whether the school is a college, by the U.S. meaning."
       }
     }
+  },
+
+  beforeSave: function(resource) {
+    return new Promise((resolve, reject) => {
+      resource.attrs.description = "Modified in a Promise";
+      resolve(resource);
+    });
   }
 };

test/integration/create-resource/index.js
@@ -1,6 +1,10 @@
 import {expect} from "chai";
 import AgentPromise from "../../app/agent";
-import {ORG_RESOURCE_CLIENT_ID, VALID_ORG_RESOURCE_NO_ID_EXTRA_MEMBER} from "../fixtures/creation";
+import {
+  ORG_RESOURCE_CLIENT_ID,
+  VALID_ORG_RESOURCE_NO_ID_EXTRA_MEMBER,
+  VALID_SCHOOL_RESOURCE_NO_ID
+} from "../fixtures/creation";
 
 describe("", (describeDone) => {
   AgentPromise.then((Agent) => {
@@ -41,6 +45,26 @@ describe("", (describeDone) => {
 
             describe("Links", () => {
 
+            });
+            
+            describe("Transforms", () => {
+              describe("beforeSave", () => {
+                it("should execute beforeSave hook", (done) => {
+                  expect(createdResource.attributes.description).to.equal("Added a description in beforeSave");
+                  done();
+                });
+
+                it("should allow beforeSave to return a Promise", (done) => {
+                  Agent.request("POST", "/schools")
+                    .type("application/vnd.api+json")
+                    .send({"data": VALID_SCHOOL_RESOURCE_NO_ID})
+                    .promise()
+                    .then((res) => {
+                      expect(res.body.data.attributes.description).to.equal("Modified in a Promise");
+                      done();
+                    }, done).catch(done);
+                });
+              });
             });
 
             describe("The Created Resource", () => {

test/integration/fixtures/creation.js
@@ -17,3 +17,10 @@ export const VALID_ORG_RESOURCE_NO_ID_EXTRA_MEMBER = Object.assign(
 export const ORG_RESOURCE_CLIENT_ID = Object.assign(
   {"id": "53f54dd98d1e62ff12539db3"}, VALID_ORG_RESOURCE_NO_ID
 );
+
+export const VALID_SCHOOL_RESOURCE_NO_ID = {
+  "type": "schools",
+  "attributes": {
+    "name": "Test School"
+  }
+};

commit 6e7b73cbc0671904fe9c6379b83757253b6cad05

Tranform Bugfix
This fixes a small bug introduced by
https://github.com/ethanresnick/json-api/commit/c2d709d73e0c668544ed926b
1aedc65d9624daaf

Basically, now that a user’s transform can return a promise, and
`transform()` always returns a promise, we have to put the `filter`
inside the `.then()`.

Closes #50

src/steps/apply-transform.js
@@ -14,9 +14,9 @@ export default function(toTransform, mode, registry, frameworkReq, frameworkRes)
     // below, allow the user to return undefined to remove a vlaue.
     return Promise.all(toTransform.resources.map((it) =>
       transform(it, frameworkReq, frameworkRes, mode, registry)
-    ).filter((it) => it !== undefined)).then((newResources) => {
-      return new Collection(newResources);
-    });
+    )).then((transformed) =>
+      new Collection(transformed.filter((it) => it !== undefined))
+    );
   }
 
   // We only transform resources or collections.