4e214cbfd752e415c8f614e23fe858d18434bb19
Ethan Resnick <ethan.resnick@gmail.com>
2015-09-09 22:12:56 -0700

    Create Resource Test: restructure
    Same story here as with the restructuring of the fetch collection tests.

--- test/integration/create-resource/index.js
+++ test/integration/create-resource/index.js
@@ -6,104 +6,115 @@ import {
   VALID_SCHOOL_RESOURCE_NO_ID
 } from "../fixtures/creation";
 
-describe("", () => {
-  AgentPromise.then((Agent) => {
-    Agent.request("POST", "/organizations")
-      .type("application/vnd.api+json")
-      .send({"data": VALID_ORG_RESOURCE_NO_ID_EXTRA_MEMBER, "extra": false})
-      .promise()
-      .then((res) => {
-        const createdResource = res.body.data;
+describe("Creating Resources", () => {
+  let Agent;
 
-        describe("Creating a Valid Resource (With an Extra Member)", () => {
-          describe("HTTP", () => {
-            it("should return 201", () => {
-              expect(res.status).to.equal(201);
-            });
-
-            it("should include a valid Location header", () => {
-              expect(res.headers.location).to.match(/\/organizations\/[a-z0-9]+/);
-              expect(createdResource.links.self).to.equal(res.headers.location);
-            });
+  describe("Creating a Valid Resource (With an Extra Member)", () => {
+    let createdResource, createdId, res;
+    before(done => {
+      AgentPromise.then((A) => {
+        Agent = A;
+        Agent.request("POST", "/organizations")
+          .type("application/vnd.api+json")
+          .send({"data": VALID_ORG_RESOURCE_NO_ID_EXTRA_MEMBER, "extra": false})
+          .promise()
+          .then((response) => {
+            res = response;
+            createdResource = res.body.data;
+            createdId = res.body.data.id;
+            done();
           });
+      }).catch(done);
+    });
 
-          describe("Document Structure", () => {
-            // "A JSON object MUST be at the root of every
-            // JSON API request and response containing data."
-            it("should have an object/document at the top level", () => {
-              expect(res.body).to.be.an("object");
-            });
+    describe("HTTP", () => {
+      it("should return 201", () => {
+        expect(res.status).to.equal(201);
+      });
 
-            it("should ignore extra document object members", () => {
-              expect(res.status).to.be.within(200, 299);
-              expect(res.body.extra).to.be.undefined;
-            });
+      it("should include a valid Location header", () => {
+        expect(res.headers.location).to.match(/\/organizations\/[a-z0-9]+/);
+        expect(createdResource.links.self).to.equal(res.headers.location);
+      });
+    });
 
-            describe("Links", () => {
+    describe("Document Structure", () => {
+      // "A JSON object MUST be at the root of every
+      // JSON API request and response containing data."
+      it("should have an object/document at the top level", () => {
+        expect(res.body).to.be.an("object");
+      });
 
-            });
+      it("should ignore extra document object members", () => {
+        expect(res.status).to.be.within(200, 299);
+        expect(res.body.extra).to.be.undefined;
+      });
+    });
 
-            describe("Transforms", () => {
-              describe("beforeSave", () => {
-                it("should execute beforeSave hook", () => {
-                  expect(createdResource.attributes.description).to.equal("Added a description in beforeSave");
-                });
+    describe("Links", () => {
 
-                it("should allow beforeSave to return a Promise", (done) => {
-                  Agent.request("POST", "/schools")
-                    .type("application/vnd.api+json")
-                    .send({"data": VALID_SCHOOL_RESOURCE_NO_ID})
-                    .promise()
-                    .then((res2) => {
-                      expect(res2.body.data.attributes.description).to.equal("Modified in a Promise");
-                      done();
-                    }, done).catch(done);
-                });
-              });
-            });
+    });
 
-            describe("The Created Resource", () => {
-              it("should return the created resource", () => {
-                expect(createdResource).to.be.an("object");
-                expect(createdResource.type).to.equal("organizations");
-                expect(createdResource.attributes).to.be.an("object");
-                expect(createdResource.relationships).to.be.an("object");
-                expect(createdResource.relationships.liaisons).to.be.an("object");
-              });
-
-              it("should ignore extra resource object members", () => {
-                expect(res.body.data.extraMember).to.be.undefined;
-                expect(res.body.data.attributes.extraMember).to.be.undefined;
-              });
-            });
-          });
+    describe("Transforms", () => {
+      describe("beforeSave", () => {
+        it("should execute beforeSave hook", () => {
+          expect(createdResource.attributes.description).to.equal("Added a description in beforeSave");
         });
-      }).done();
-  }).done();
-});
 
-describe("", () => {
-  AgentPromise.then((Agent) => {
-    Agent.request("POST", "/organizations")
-      .type("application/vnd.api+json")
-      .send({"data": ORG_RESOURCE_CLIENT_ID})
-      .promise()
-      .then(() => { throw new Error("Should not run!"); }, (err) => {
-        describe("Creating a Resource With A Client-Id", () => {
-          describe("HTTP", () => {
-            it("should return 403", () => {
-              expect(err.response.status).to.equal(403);
-            });
-          });
-
-          describe("Document Structure", () => {
-            it("should contain an error", () => {
-              expect(err.response.body.errors).to.be.an("array");
-            });
-          });
+        it("should allow beforeSave to return a Promise", (done) => {
+          Agent.request("POST", "/schools")
+            .type("application/vnd.api+json")
+            .send({"data": VALID_SCHOOL_RESOURCE_NO_ID})
+            .promise()
+            .then((response) => {
+              expect(response.body.data.attributes.description).to.equal("Modified in a Promise");
+              done();
+            }, done).catch(done);
         });
-      }).done();
-  }).done();
+      });
+    });
+
+    describe("The Created Resource", () => {
+      it("should be returned in the body", () => {
+        expect(createdResource).to.be.an("object");
+        expect(createdResource.type).to.equal("organizations");
+        expect(createdResource.attributes).to.be.an("object");
+        expect(createdResource.relationships).to.be.an("object");
+        expect(createdResource.relationships.liaisons).to.be.an("object");
+      });
+
+      it("should ignore extra resource object members", () => {
+        expect(res.body.data.extraMember).to.be.undefined;
+        expect(res.body.data.attributes.extraMember).to.be.undefined;
+      });
+    });
+  });
+
+  describe("Creating a Resource With A Client-Id", () => {
+    let err;
+    before(done => {
+      Agent.request("POST", "/organizations")
+        .type("application/vnd.api+json")
+        .send({"data": ORG_RESOURCE_CLIENT_ID})
+        .promise()
+        .then(() => done("Should not run!"), (error) => {
+          err = error;
+          done();
+        });
+    });
+
+    describe("HTTP", () => {
+      it("should return 403", () => {
+        expect(err.response.status).to.equal(403);
+      });
+    });
+
+    describe("Document Structure", () => {
+      it("should contain an error", () => {
+        expect(err.response.body.errors).to.be.an("array");
+      });
+    });
+  });
 });
 
     // "[S]erver implementations MUST ignore
