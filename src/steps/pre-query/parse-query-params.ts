5e5c6ff9bebe74627fd5088a8aaeea1bcb354a08
Ethan Resnick <ethan.resnick@gmail.com>
2018-01-07 22:20:58 -0500

    chore: deps upgrades, misc cleanup

--- src/steps/pre-query/parse-query-params.ts
+++ src/steps/pre-query/parse-query-params.ts
@@ -18,6 +18,7 @@ export type ParsedQueryParams = {
   sort?: Sort[];
   page?: ScopedParam;
   fields?: ScopedStringListParam;
+  [paramName: string]: any;
 }
 
 export default function(params: RawParams): ParsedQueryParams {
