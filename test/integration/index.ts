f9588a0de02caf837c3c006a1c909c6e98480171
Ethan Resnick <ethan.resnick@gmail.com>
2018-01-14 17:55:31 -0800

    tests: trigger integration tests with fixtures
    By pointing mocha to index.ts, and having that run all the other tests,
    we can make sure the fixtures run at the start of each test run. I
    guess they weren’t before?

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -1,23 +1,25 @@
 import Db from "../app/database";
 
-before((done) => {
-  Db.then((module) => {
+before(() => {
+  return Db.then((module) => {
     return module.fixturesReset().then((data) => {
       // Trigger other tests.
       // Note: we must load these after the fixtures finish, since even
       // _describing_ the require()d tests needs the fixtures: the App
       // requests come before describe the describe calls, and those
       // requests will fail if the fixtures aren't in place.
-      require("./http-compliance");
       require("./content-negotiation");
-      require("./fetch-collection");
       require("./create-resource");
-      require("./patch-relationship");
+      require("./custom-query");
       require("./delete-resource");
       require("./documentation");
-      done();
+      require("./error-handling");
+      require("./fetch-collection");
+      require("./fetch-relationship");
+      require("./fetch-resource");
+      require("./http-compliance");
+      require("./patch-relationship");
+      require("./update-resource");
     });
-  }).catch(done);
+  })
 });
-
-
