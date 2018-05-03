660f93fb0ec68fa859c822e8ecc3c9741ec1a13b
Ethan Resnick <ethan.resnick@gmail.com>
2018-05-02 21:20:32 -0700

    fix(typings): tweak typings to work with ts@next
    See https://github.com/Microsoft/TypeScript/pull/23592

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,9 +1,6 @@
 import APIError, { Opts } from '../types/APIError';
 
-export type Diff<T extends string, U extends string> =
-  ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
-
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 export type ErrorOpts = Omit<Opts, "title" | "status" | "typeUri" | "code">;
 
 /*
